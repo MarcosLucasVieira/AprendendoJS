97393d9c4061b5b24230bbc44bed8d6978f984aa
Marcos Lucas Vieira <doublestrike22dh@gmail.com>
2022-05-17 16:50:50 -0300

    Atualizando aula de Array

--- Array.js/aula16(array01).js
+++ Array.js/aula16(array01).js
@@ -1,4 +1,38 @@
+//                0        1        2          3        4
+const alunos = ['Luiz', 'Maria', 'João']; // luiza, eduardo
+
+
+ console.log(typeof alunos);
+ console.log(alunos instanceof Array);
+
+ alunos.push('luiza');
+ alunos.push('Eduardo');
+
+
+delete aluno [1]; // para remover um elemento e não mudar o indice da array
+ const removido = alunos.shift();// para remover o primeiro elemento da array
+// const removido = alunos.pop(); //para remover o ultimo elemento da array
+console.log(removido);
+
+
+alunos.unshift('luiza'); // Adiciona ao começo
+alunos.unshift('Fábio');
+
+
+
+alunos.push('Luiza'); // Adiciona no fim.
+alunos.push('Fábio');
+alunos[alunos.length] = 'Marcos'; 
+alunos[alunos.length] = 'luiza';
+alunos[alunos.length] = 'fabio';
+
+
+
+
 const nomes ='Luiz Otávio';
-const alunos = ['Luiz', 'Maria', 'João', 1,true, null];
 console.log(alunos);
 console.log(alunos[0]);
+
+alunos [3]= 'luiza'; //adiciona
+alunos [0]= 'eduardo'; //altera
+
